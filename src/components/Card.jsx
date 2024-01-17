9cbc6cb932df83a8ec90289102661d7e3a34e051
Dk007ms <dkaushik290@gmail.com>
2024-01-18 01:23:01 +0530

    Fixes:media queries

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,12 +8,12 @@ export default function Card(props) {
     let rev=props.data;
     console.log(rev);
   return (
-    <div className='card flex flex-col justify-center  items-center w-full h-max relative gap-2 xl:gap-12 xl:static' >
-      <img className="testimonial w-36 h-36 xl:w-36 xl:h-36 object-cover rounded-full relative xl:absolute xl:left-[28rem] xl:top-40 " src={rev[index].image} alt={rev[index].name} />
+    <div className='card flex flex-col justify-center items-center w-full h-max relative gap-2 xl:gap-4' >
+      <img className="testimonial w-36 h-36 xl:w-36 xl:h-36 object-cover rounded-full relative" src={rev[index].image} alt={rev[index].name} />
       <div className='mediaquery text-black xl:text-5xl text-3xl font-bold font-mono'>
         {rev[index].name}
       </div>
-      <p className='mediaquery review w-4/6 relative xl:left-6 text-center font-serif xl:text-lg '>{rev[index].text}</p>
+      <p className='mediaquery review w-4/6 relative text-center font-serif xl:text-lg '>{rev[index].text}</p>
       <div className="buttons mediaquery flex flex-col items-center justify-center w-11/12 gap-4">
         <div className="caraousel_effect flex justify-evenly w-3/6 h-10">
         <button className="prev bg-black text-white w-8 h-8 flex justify-center items-center rounded-full"><GrPrevious/></button>
--- src/components/Testimonialcontainer.jsx
+++ src/components/Testimonialcontainer.jsx
@@ -5,7 +5,7 @@ import data from './data'
 export default function TestimonialContainer() {
     
   return (
-    <div className=' bg-red-50 w-11/12 h-max xl:w-3/6 xl:h-4/6 rounded-lg p-4 xl:flex xl:items-center'>
+    <div className=' bg-red-50 w-11/12 h-max xl:w-7/12 rounded-lg p-4'>
       <Card data={data}></Card>
     </div>
   )
